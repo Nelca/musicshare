1a9186076b6d5ffd6f39d952092033d9c3e98f9f
Nelca <m.n.93710167@gmail.com>
2016-08-06 13:17:24 +0000

    add playlist test case

--- tests/PlaylistTest.php
+++ tests/PlaylistTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
 
 class PlaylistTest extends TestCase
 {
@@ -18,10 +19,42 @@ class PlaylistTest extends TestCase
 
     public function testViewPlaylistsUser()
     {
-        $user = factory(App\User::class)->create();
+        $user = factory(User::class)->create();
 
 	$this->actingAs($user)
 	     ->visit('/playlists')
 	     ->see('Current Playlists');
+    }
+
+    public function testViewSongs()
+    {
+        $user = factory(User::class)->create();
+
+	$this->actingAs($user)
+	     ->visit('/playlists')
+	     ->press('View Songs')
+	     ->seePageIs('/playlist/1/songs');
+    }
+
+    public function testBlankPlaylist()
+    {
+        $user = factory(User::class)->create();
+
+	$this->actingAs($user)
+	     ->visit('/playlists')
+	     ->press('Add Playlist')
+	     ->see('name field is required');
+    }
+
+    public function testAddPlaylist()
+    {
+        $user = factory(User::class)->create();
+	$insertPlaylistName = 'unit test';
+
+	$this->actingAs($user)
+	     ->visit('/playlists')
+	     ->type($insertPlaylistName, 'name')
+	     ->press('Add Playlist')
+	     ->see($insertPlaylistName);
     }
 }
