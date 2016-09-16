375263cc60519519be16c6fdefec42104f25865d
Nelca <m.n.93710167@gmail.com>
2016-09-16 14:31:06 +0000

    modify mypage order

--- app/Http/Controllers/MyPageController.php
+++ app/Http/Controllers/MyPageController.php
@@ -14,27 +14,30 @@ class MyPageController extends Controller
     {
         $user_id = $request->user()->id;
 
-	$query = " select s.id, s.name, s.url, s.song_key, 'song' as type from songs as s ";
+	$query = " SELECT s.id, s.name, s.url, s.song_key, 'song' as type, s.updated_at as updated_at ";
+	$query .= " from songs as s ";
 	$query .= " inner join playlists as p ON s.playlist_id = p.id ";
 	$query .= " inner join users as u on u.id = p.user_id ";
 	$query .= " where u.id IN (";
 	$query .= " select u2.id FROM users as u2 ";
 	$query .= " inner join follows as f on u2.id = f.follow_user_id ";
 	$query .= " WHERE f.user_id = ?)";
-	$query .= " UNION ALL";
-	$query .= " select fav.id, fav.name, fav.url, fav.song_key, 'favorite' as type from favorites as fav ";
+	$query .= " UNION ALL ";
+	$query .= " select fav.id, fav.name, fav.url, fav.song_key, 'favorite' as type, fav.updated_at as updated_at ";
+	$query .= " from favorites as fav ";
 	$query .= " inner join users as u3 on u3.id = fav.user_id";
 	$query .= " where u3.id IN (";
 	$query .= " select u4.id FROM users as u4";
 	$query .= " inner join follows as f2 on u4.id = f2.follow_user_id ";
 	$query .= " WHERE f2.user_id = ?)";
-	$query .= " UNION ALL";
-	$query .= " SELECT s.id, s.name, s.url, s.song_key, 'follow' as type ";
+	$query .= " UNION ALL ";
+	$query .= " SELECT s.id, s.name, s.url, s.song_key, 'follow' as type, e.updated_at as updated_at ";
 	$query .= " FROM evaluates as e";
 	$query .= " JOIN songs as s ON s.id = e.evaluatable_id";
 	$query .= " WHERE e.user_id IN (select u4.id FROM users as u4";
 	$query .= " inner join follows as f2 on u4.id = f2.follow_user_id ";
 	$query .= " WHERE f2.user_id = ?)";
+	$query .= " ORDER BY updated_at DESC";
 
 	$songs = DB::select($query, [$user_id, $user_id, $user_id]);
         return view('mypage.index', [
