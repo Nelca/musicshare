5a5fa35b91741681d5cce5e1ae80155c1bcea5a7
Nelca <m.n.93710167@gmail.com>
2016-11-04 17:02:21 +0000

    remove mypage favorite

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -4,41 +4,6 @@
     <div class="container">
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
-                <div class="panel-heading">
-                    New Song
-                </div>
-                <div class="panel-body">
-                    <!-- Display Validation Errors -->
-                    @include('common.errors')
-
-                    <!-- New Favorite Form -->
-                    <form action="{{ url('favorite') }}" method="POST" class="form-horizontal">
-                        {{ csrf_field() }}
-
-                        <!-- Favorie Name -->
-                        <div class="form-group">
-                            <label for="favorie-name" class="col-sm-3 control-label">Favorite</label>
-                            <div class="col-sm-6">
-                                <input type="text" name="name" id="favorite-name" class="form-control" value="{{ old('favorite') }}">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="favorie-url" class="col-sm-3 control-label">URL</label>
-                            <div class="col-sm-6">
-                                <input type="text" name="url" id="favorite-url" class="form-control" value="{{ old('favorite') }}">
-                            </div>
-                        </div>
-
-                        <!-- Add Favorite Button -->
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-6">
-                                <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-btn fa-plus"></i>Add Favorite 
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
                <div class="panel panel-default">
                    <div class="panel-heading">
                        <i class="fa fa-users" aria-hidden="true"></i>Follow
