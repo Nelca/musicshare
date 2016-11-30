53ec606238619a5f03af2f84274391f76b6efa94
Nelca <m.n.93710167@gmail.com>
2016-11-30 16:00:41 +0000

    modify song test

--- tests/SongTest.php
+++ tests/SongTest.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\User;
+//use Youtube;
 
 class SongTest extends TestCase
 {
@@ -25,5 +26,22 @@ class SongTest extends TestCase
              ->type($songName, 'name')
              ->press('Add Song')
              ->see($songName);
+    }
+
+    public function testAddSongWithName()
+    {
+        $user = factory(User::class)->create();
+
+        $songKey = 'ZABXtVxyJwg&t=137s';
+        //$songData = Youtube::getVideoInfo($songKey);
+        //$songName = $songData->snippet->title; 
+        $songUrl = "https://www.youtube.com/watch?v=" . $songKey;
+
+        $this->actingAs($user)
+             ->visit('/playlists')
+             ->press('View Songs')
+             ->type($songUrl, 'url')
+             ->press('Add Song');
+
     }
 }
