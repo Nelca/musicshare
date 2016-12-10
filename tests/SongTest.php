aa852d09179265840dd81e0220aa65764c6fde8f
Nelca <m.n.93710167@gmail.com>
2016-12-10 12:56:16 +0000

    modify song test

--- tests/SongTest.php
+++ tests/SongTest.php
@@ -4,11 +4,12 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\User;
+use App\Playlist;
+use App\Song;
 //use Youtube;
 
 class SongTest extends TestCase
 {
-    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -47,20 +48,35 @@ class SongTest extends TestCase
     public function testSongLikeUnlike()
     {
         $user = factory(User::class)->create();
-        $this->actingAs($user)
-             ->visit('/playlist/12/songs')
-             ->press('like-song-28')
+        $uid = $user->id;
+
+        $playlist = Playlist::create([
+            'name' => 'test_playlist'
+            , 'user_id' => $uid 
+        ]);
+        $pid = $playlist->id;
+
+        $song = Song::create([
+            'playlist_id' => $pid
+            , 'name' => 'test_song'
+            , 'url' => 'https://www.youtube.com/watch?v=3FzNpto-jnU'
+        ]);
+        $sid = $song->id;
+
+       $this->actingAs($user)
+             ->visit('/playlist/' . $pid . '/songs')
+             ->press('like-song-' . $sid)
              ->seeInDatabase('evaluates',[
-                 'user_id' => $user->id
+                 'user_id' => $uid
                  , 'evaluation' => 1
-                 , 'evaluatable_id' => 28
+                 , 'evaluatable_id' => $sid
                  , 'evaluatable_type' => 'songs'
              ]);
              
         $this->actingAs($user)
-             ->visit('/playlist/12/songs')
-             ->press('liked-song-28')
-             ->seePageIs('/playlist/12/songs');
+             ->visit('/playlist/' . $pid . '/songs')
+             ->press('liked-song-' . $sid)
+             ->seePageIs('/playlist/' . $pid . '/songs');
 
     }
 }
