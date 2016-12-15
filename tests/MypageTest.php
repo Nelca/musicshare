974f8c822b839ef95df02a86775cf0e7411d5415
Nelca <m.n.93710167@gmail.com>
2016-12-15 12:53:03 +0000

    modify mypage test

--- tests/MypageTest.php
+++ tests/MypageTest.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\User;
+use App\Playlist;
+use App\Song;
 
 class MypageTest extends TestCase
 {
@@ -27,5 +29,38 @@ class MypageTest extends TestCase
         $this->actingAs($user)
             ->visit('/mypage/likes')
             ->see('There is nothing yet.');
+    }
+
+    public function testSongLikeUnlike()
+    {
+        $user = factory(User::class)->create();
+        $uid = $user->id;
+
+        $playlist = $user->playlists()->create([
+            'name' => 'test_playlist'
+        ]);
+        $pid = $playlist->id;
+
+        $songName = 'test_song';
+        $song = new Song;
+        $song->url = 'https://www.youtube.com/watch?v=3FzNpto-jnU';
+        $song->name = $songName;
+
+        $playlist->songs()->save($song);
+
+        $savedSong = Song::where('name', $songName)->first();
+        $sid = $savedSong->id;
+
+        $this->actingAs($user)
+            ->visit('/playlistis')
+            ->press('like-playlist-')
+            ->visit('/mypage/likes')
+            ->see($playlist->name);
+
+        $this->actingAs($user)
+            ->visit('/playlist/' . $pid . '/songs')
+            ->press('like-song-' . $sid)
+            ->visit('/mypage/likes')
+            ->see($songName);
     }
 }
