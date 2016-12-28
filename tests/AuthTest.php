5ee432142c488a7f88189625863f2e01041fcf6c
Nelca <m.n.93710167@gmail.com>
2016-12-28 14:04:45 +0000

    add user regist test

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -7,7 +7,12 @@ use App\User;
 
 class AuthTest extends TestCase
 {
-    //use DatabaseTransactions;
+    use DatabaseTransactions;
+
+    private $testUserName = "testUser";
+    private $testUserEmail = "asdf@asdf.jp";
+    private $testUserPassword = "password";
+    private $testUserEncodedPass = '$2y$10$XtSdROCcDTG7XuaC.4A0reMY/hD/cOmV8txPkUdRvUbNgZqgjPad.';
 
     public function testLoginValidate()
     {
@@ -15,7 +20,7 @@ class AuthTest extends TestCase
             ->press('Login')
             ->see('The email field is required.')
             ->see('The password field is required.')
-            ->type('test@test.jp', 'email')
+            ->type($this->testUserEmail, 'email')
             ->press('Login')
             ->see('The password field is required.')
             ->type('', 'email')
@@ -23,18 +28,40 @@ class AuthTest extends TestCase
             ->press('Login')
             ->see('The email field is required.');
     }
-
     public function testUserLogin()
     {
         $user = new User();
-        $user->name = "testUser";
-        $user->email = "test_b@test.jp";
-        $user->password = '$2y$10$XtSdROCcDTG7XuaC.4A0reMY/hD/cOmV8txPkUdRvUbNgZqgjPad.';
+        $user->name = $this->testUserName;
+        $user->email = $this->testUserName;
+        $user->password = $this->testUserEncodedPass;
         $user->save();
         $this->visit('/login')
             ->type($user->email, 'email')
-            ->type("password", 'password')
+            ->type($this->testUserPassword, 'password')
             ->press('Login')
             ->seePageIs('/mypage');
+    }
+
+    public function testRegistValidate()
+    {
+        $this->visit('/register')
+            ->press('Register')
+            ->see('The name field is required.')
+            ->see('The email field is required.')
+            ->see('The password field is required.');
+    }
+    public function testRegistUser()
+    {
+        $this->visit('/register')
+            ->type($this->testUserName, 'name')
+            ->type($this->testUserEmail, 'email')
+            ->type($this->testUserPassword, 'password')
+            ->type($this->testUserPassword, 'password_confirmation')
+            ->press('Register')
+            ->seePageIs('/mypage')
+            ->seeInDatabase('users',[
+                'name' => $this->testUserName
+                , 'email' => $this->testUserEmail
+            ]);
     }
 }
