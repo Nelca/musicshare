128806be0739e4bd8a2d6d72d4d72efe811d0eb0
Nelca <m.n.93710167@gmail.com>
2017-01-27 23:10:48 +0000

    modify mypage

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -10,10 +10,14 @@
                <div class="panel-body">
                     <div class="row">
                         <div class="col-xs-12 col-md-6">
-                            <a href="{{ url('user/' . $user->id . '/follow') }}">{{ count($follow) }}  Follow</a>
+                            <a href="{{ url('user/' . $user->id . '/follow') }}">
+                                <i class="fa fa-users" aria-hidden="true"></i>&nbsp;{{ count($follow) }}  Follow
+                            </a>
                         </div>
                         <div class="col-xs-12 col-md-6">
-                            <a href="{{ url('user/' . $user->id . '/follower') }}">{{ count($follower) }}  Follower</a>
+                            <a href="{{ url('user/' . $user->id . '/follower') }}">
+                                <i class="fa fa-users" aria-hidden="true"></i>&nbsp;{{ count($follower) }}  Follower
+                            </a>
                         </div>
                     </div>
                 </div>
