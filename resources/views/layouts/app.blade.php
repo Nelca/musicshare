d7752cbadd4d03391f7e4aa057b03e71f129fec8
Nelca <m.n.93710167@gmail.com>
2017-01-28 19:25:27 +0000

    modify toppage icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                <i class="fa fa-user" area-hidden="true"></i>{{ Auth::user()->name }} <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/mypage') }}"><i class="fa fa-btn fa-home"></i>MyPage</a></li>
